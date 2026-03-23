9f22db3881af9971a2ade8722df24b354cb8d0ca
Simon J. Larsen <sjl@esoft.com>
2026-03-23 11:05:26 +0000

    add noise.nvim and disable snacks/notify

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,9 +6,6 @@ return {
     opts = {
         bufdelete = { enabled = true },
         indent = { enabled = true },
-        input = { enabled = true },
-        notifier = { enabled = true },
-        scroll = { enabled = true },
         statuscolumn = { enabled = true },
     },
 }
