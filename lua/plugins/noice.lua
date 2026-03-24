9618044792fcbbc8dc7af40f463ac9533fa87925
Simon J. Larsen <sjl@esoft.com>
2026-03-24 14:26:25 +0000

    Tweak noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -23,10 +23,11 @@ return {
         },
         presets = {
             bottom_search = true, -- use a classic bottom cmdline for search
+            lsp_doc_border = false, -- add a border to hover docs and signature help
             command_palette = true, -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
             inc_rename = false, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = false, -- add a border to hover docs and signature help
+            cmdline_output_to_split = false,
         },
     },
     dependencies = {
