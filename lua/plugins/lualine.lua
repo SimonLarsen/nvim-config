93fa41db9a7a00dfb759a187286dd28d11f91866
Simon J. Larsen <sjl@esoft.com>
2026-04-10 11:52:23 +0000

    Configure lualine to work better with ui2

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,32 @@
+local function macro()
+    local reg = vim.fn.reg_recording()
+    if reg == "" then
+        return ""
+    else
+        return "recording to " .. reg
+    end
+end
+
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {},
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        extensions = {"neo-tree", "trouble"},
+        sections = {
+            lualine_a = {"mode", macro},
+            lualine_b = {"branch", "diff", "diagnostics"},
+            lualine_c = {"filename"},
+            lualine_x = {"encoding", "fileformat", "filetype"},
+            lualine_y = {"progress"},
+            lualine_z = {"location"}
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {"filename"},
+            lualine_x = {"location"},
+            lualine_y = {},
+            lualine_z = {}
+        },
+    },
 }
