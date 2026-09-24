851761337f04a2ab4b7028400d526140d5d10dd5
Simon J. Larsen <simonhffh@gmail.com>
2026-09-24 14:20:32 +0200

    Update plugins, use blink.cmp adapter in tiny-cmdline

--- lua/plugins/tiny-cmdline.lua
+++ lua/plugins/tiny-cmdline.lua
@@ -1,5 +1,8 @@
 return {
     "rachartier/tiny-cmdline.nvim",
+    opts = {
+        on_reposition = require("tiny-cmdline").adapters.blink,
+    },
     init = function()
         vim.o.cmdheight = 0
     end,
